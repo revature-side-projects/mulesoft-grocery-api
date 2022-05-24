1e94fc6669f5b21557f443f86f53155cf9692957
IkenoXamos <moberlies1337@gmail.com>
2022-05-24 17:24:07 -0400

    Updated mock data and added swagger

--- src/main/java/com/revature/MulesoftGroceryApiApplication.java
+++ src/main/java/com/revature/MulesoftGroceryApiApplication.java
@@ -1,9 +1,12 @@
 package com.revature;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Grocery API", version = "1.0", description = "Grocery API"))
 public class MulesoftGroceryApiApplication {
 
 	public static void main(String[] args) {
