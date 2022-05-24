0f3dcf4d6e30b30ce06872f5bd29ed4b1fea2f89
IkenoXamos <moberlies1337@gmail.com>
2022-05-24 17:24:30 -0400

    Cleanup

--- src/main/java/com/revature/models/Order.java
+++ src/main/java/com/revature/models/Order.java
@@ -1,9 +1,5 @@
 package com.revature.models;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,8 +25,6 @@ public class Order {
     private int quantity;
 
     @Column(name = "datetime")
-//    @JsonDeserialize(using = LocalDateDeserializer.class)
-//    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate date;
 
     @Column(name = "store_id")
